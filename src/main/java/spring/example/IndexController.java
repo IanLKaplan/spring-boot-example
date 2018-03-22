89e854a2f72f2f8850babf0f9dbb14d255339a2c
iank <iank@bearcave.com>
2018-03-22 17:06:12 -0400

    Changed the Map argument in the index method in the IndexController class to Model, which is more standard for the Spring framework.

--- src/main/java/spring/example/IndexController.java
+++ src/main/java/spring/example/IndexController.java
@@ -10,9 +10,9 @@ package spring.example;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -22,11 +22,11 @@ public class IndexController {
      * Controller function for the index HTML page.
      */
     @RequestMapping("/")
-    public String index(Map<String, Object> model) {
+    public String index(Model model) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("MMM d yyyy : HH:mm:ss:SSS z");
         Date now = new Date();
         String dateStr = dateFormat.format( now );
-        model.put("time", dateStr);
+        model.addAttribute("time", dateStr);
         return "index";
     }
 
